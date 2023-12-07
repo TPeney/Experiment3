3f5458b1cb79fbb16726ba732f2a9fdf9dbf5bc8
Tom Peney <tompeney@hotmail.co.uk>
2023-12-07 21:33:40 +0000

    Initial commit of data import and cleaning script

---
+++ Analysis/data_import_and_clean.R
@@ -0,0 +1,30 @@
+require(tidyverse)
+
+files <- list.files(
+  path = "./data_raw/behavioural_data",
+  pattern = "trial_results.csv",
+  full.names = TRUE,
+  recursive = TRUE
+)
+
+data.raw <- bind_rows(lapply(files, read_csv,
+  col_names = TRUE,
+  # Quiet a warning related to auto col_type detection
+  show_col_types = FALSE,
+  # Explicitly specify the data types of certain cols
+  cols(ppid = col_integer(), condition = col_character())
+))
+
+data.cleaned <- data.raw |>
+  # Remove practice trials
+  filter(blockType != "practice") |>
+  # Remove redundant columns
+  select(experiment, ppid, session_num, trial_num_in_block, RT, trialType, arraySize, targetID, trialPassed, condition) |>
+  # Transform from sec to ms
+  mutate(RT = RT * 1000) |>
+  # Sets an 'intrinsic order' for the conditions
+  mutate(condition = factor(condition, levels = c("2D", "VRH", "VRG"))) |>
+  # Remove erroneous data
+  filter(RT > 0)
+
+saveRDS(data.cleaned, file = "./data_processed/aggregated_behavioural_data") 
